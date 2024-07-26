3b87d931c081d3743f8810c8a7c1c2ca9e6327c1
Joseph Okpetu <iamemperor02@gmail.com>
2024-07-26 12:48:48 +0100

    Initial commit

---
+++ app/Http/Controllers/HelpSupportController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelpSupportController extends Controller
+{
+    public function index()
+    {
+        return view('help_support/index');
+    }
+
+    public function faqs()
+    {
+        return view('help_support/faqs');
+    }
+
+    public function help_documentation()
+    {
+        return view('help_support/help_documentation');
+    }
+
+    public function support_contact()
+    {
+        return view('help_support/support_contact');
+    }
+}
